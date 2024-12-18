8f2406722a48a44804b285e2124a63eb5ad33c2b
alsekaram <git@awl.su>
2024-12-18 21:38:30 +0300

    Set up secure cookie settings and load environment variables
    Introduced `CSRF_COOKIE_SECURE` and `SESSION_COOKIE_SECURE` for enhanced security. Added loading of environment variables via `dotenv` for better secret management. Ensured production settings prioritize security while maintaining flexibility for local development.

--- perfumancer/perfumancer/settings/prod.py
+++ perfumancer/perfumancer/settings/prod.py
@@ -1,4 +1,12 @@
+from django.conf.global_settings import SESSION_COOKIE_SECURE, SECURE_SSL_REDIRECT, SECURE_HSTS_SECONDS
+import os
+from dotenv import load_dotenv
 from .base import *
+
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 DEBUG = False
 
@@ -14,3 +22,10 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3'
     }
 }
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = False
+SECURE_HSTS_INCLUDE_SUBDOMAINS = False
+SECURE_HSTS_SECONDS = 0
+SECURE_HSTS_PRELOAD = False
