ff2cf89ae11e564c1447c49d9587bf1e325a0a12
alsekaram <git@awl.su>
2025-06-16 14:10:42 +0300

    Switch to new Ngrok URL and standardize string formatting in local settings

--- perfumancer/perfumancer/settings/local.py
+++ perfumancer/perfumancer/settings/local.py
@@ -4,20 +4,17 @@ DEBUG = True
 
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://7166-116-203-69-235.ngrok-free.app",
+    "https://c149-116-203-69-235.ngrok-free.app",
 ]
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3'
-    }
+    "default": {"ENGINE": "django.db.backends.sqlite3", "NAME": BASE_DIR / "db.sqlite3"}
 }
 
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_BROKER_URL = "redis://localhost:6379/0"
+CELERY_RESULT_BACKEND = "redis://localhost:6379/0"
 CELERY_TASK_TRACK_STARTED = True
 CELERY_TASK_RESULT_EXPIRES = 3600
